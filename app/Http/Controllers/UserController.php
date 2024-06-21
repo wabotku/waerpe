3c527f262aa9615baf3b1138eccdc406326f5710
walid <mohamad.walid@sby.spil>
2024-06-21 15:06:23 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login(){
+        dd('login');
+    }
+
+    public function register(){
+        dd('register');
+    }
+}
---
+++ app/Http/Middleware/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class auth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // dd($request);
+        // if ($request->input('token') !== 'my-secret-token') {
+        //     return redirect('/user/login');
+        // }
+
+        return $next($request);
+    }
+}
